85e58eb0d64ae3d2d6a766e1581373a62768f3b5
Isradias <israelcaldas10@gmail.com>
2026-05-01 05:30:54 -0300

    First

--- Back/MeuPrimeiroServidor.js
+++ Back/MeuPrimeiroServidor.js
@@ -1,33 +1,48 @@
-const express = require("express"); //express vira a classe, require é o import
-const app = express(); // express() cria e retorna o objeto, app vira objeto
-const path = require("path"); //trabalha caminhos rel e abs, ja vem com o node
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("postgres", "postgres", "3381", {
-	host: "localhost",
-	dialect: "postgres"
-});
+import express from "express";
+import path from "path";
+import sql from "./db.js";
+import bcrypt from "bcrypt";
+
+const app = express();
+app.use(express.static(path.resolve("../Front")));
+app.use(express.json());
 
 app.get("/", function (req, res) {
-	res.sendFile(path.join(__dirname, "../Front/index.html"));
+	res.sendFile(path.resolve("../Front/Home/index.html"));
 });
 
-app.get("/cadastrar", async function (req, res) {
-    try {
-        await sequelize.query(`
-            INSERT INTO cadastro (nome, sobrenome, idade, email, senha)
-            VALUES ('Israel', 'Dias', 25, 'israelcaldas10@.com', '123')
-        `);
-
-        res.send("Inserido direto no SQL 😎");
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).send("Deu ruim 😢");
-    }
+app.get("/cadastro", function (req, res) {
+	res.sendFile(path.resolve("../Front/Cadastro/cadastro.html"));
 });
-//Função de callback é executada sempre que um evento acontece
-//A maioria das funções do express tem callback
-//No caso da função abaixo é essa arrow function
+
+app.post("/cadastrar", async function (req, res) {
+	const {nick, email, senha} = req.body
+
+	const hash = await bcrypt.hash(senha, 10);
+	try {
+		await sql`
+            INSERT INTO usuarios (nick, email, senha)
+            VALUES (${nick}, ${email}, ${hash})
+        `;
+		res.send("Cadastro realizado com sucesso!");
+	} catch (err) {
+		console.log(err);
+		res.status(500).send("Deu ruim 😢");
+	}
+});
+
+app.get("/delete", async function (req, res) {
+	try {
+		await sql`
+            TRUNCATE TABLE usuarios
+        `;
+		res.send("<h1>Tudo deletado</h1>");
+	} catch (err) {
+		console.log(err);
+		res.status(500).send("Erro ao deletar");
+	}
+});
+
 app.listen(3000, function () {
 	console.log("Rodando na porta 3000");
 });
---
+++ Front/Cadastro/cadastro.js
@@ -0,0 +1,20 @@
+        async function cadastrar() {
+            const nick = document.getElementById("nick").value;
+            const email = document.getElementById("email").value;
+            const senha = document.getElementById("senha").value;
+
+            const resposta = await fetch("/cadastrar", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    nick,
+                    email,
+                    senha
+                })
+            });
+
+            const texto = await resposta.text();
+            alert(texto);
+        }
