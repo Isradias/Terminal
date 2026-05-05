dd25296cff11b3c20b79cb9582c6943dea5581f6
Isradias <israelcaldas10@gmail.com>
2026-05-05 08:47:23 -0300

    Criação de elementos dinamicamente no hub, bem como criação de um arquivo chamado lista_missoes que contem uma lista de JSON contendo informações das missões que aparecem na pagina de hub

--- Front/Fases/hub.js
+++ Front/Fases/hub.js
@@ -1,4 +1,6 @@
-async function carregar_nivel(nivel) {
+import missoes from "./lista_missoes.js";
+
+function carregar_nivel(nivel) {
 	const nivel_titulo = document.getElementById("nivel");
 	nivel_titulo.textContent = `NIVEL: ${nivel}`;
 }
@@ -18,12 +20,68 @@ async function get_user() {
 	}
 }
 
-async function carregar_nick(nick) {
+function carregar_nick(nick) {
 	const nick_titulo = document.getElementById("nick");
 	nick_titulo.textContent = nick;
 }
 
-async function subir_nivel() {
+window.logout = async function logout() {
+	try {
+		const response = await fetch("/logout", { method: "POST" });
+		if (!response.ok) {
+			console.log("Erro no logout");
+			return;
+		}
+		alert("Você saiu da sua conta");
+		window.location.href = "/";
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+function criar_missoes(nivel) {
+	const template = document.getElementById("missao_template");
+	const mapa = document.getElementById("id_mapa");
+
+	const entradas_encontradas = document.getElementById(
+		"entradas_encontradas",
+	);
+	entradas_encontradas.textContent = `${missoes.length} ENTRADAS ENCONTRADAS`;
+
+	let clone;
+
+	for (let i = 0; i < missoes.length; i++) {
+		clone = template.content.cloneNode(true);
+		clone.querySelector(".fase").innerHTML =
+			`Fase ${i + 1} <span>${missoes[i].dificuldade}</span>`;
+		clone.querySelector(".titulo_missao").textContent = missoes[i].titulo;
+		clone.querySelector(".subtitulo").textContent = missoes[i].subtitulo;
+		if (i < nivel) {
+			clone.querySelector(".status").innerHTML = `
+			<span class="material-symbols-outlined" style="color: #36e270;">
+			check_circle
+			</span>
+			`;
+		} else if (i === nivel) {
+			clone.querySelector(".status").innerHTML = `
+			<span class="material-symbols-outlined" style="color: #3899FA">
+			arrow_forward_ios
+			</span>
+			`;
+		} else {
+			clone.querySelector(".btn_missao").style.backgroundColor = "#0F1114";
+			clone.querySelector(".btn_missao").style.color = "#7A7A7B";
+			clone.querySelector(".status").innerHTML = `
+			<span class="material-symbols-outlined">
+			lock
+			</span>
+			`;
+		}
+		mapa.appendChild(clone);
+	}
+}
+
+window.subir_nivel = async function subir_nivel() {
 	try {
 		const response = await fetch("/subir_nivel", {
 			method: "PUT",
@@ -39,28 +97,15 @@ async function subir_nivel() {
 	} catch (err) {
 		console.log(err);
 	}
-}
-
-async function logout() {
-	try {
-		const response = await fetch("/logout", {method: "POST"})
-		if(!response.ok){
-			console.log("Erro no logout");
-			return			
-		}
-		alert("Você saiu da sua conta")
-		window.location.href = "/"
-	} catch (error) {
-		console.log(error);
-	}
-}
+};
 
 async function main() {
 	const user = await get_user();
-	const nick = user.nick
-	const nivel = user.nivel
+	const nick = user.nick;
+	const nivel = user.nivel;
 	carregar_nivel(nivel);
 	carregar_nick(nick);
+	criar_missoes(Number(nivel));
 }
 
 main();
---
+++ Front/Fases/lista_missoes.js
@@ -0,0 +1,87 @@
+const missoes = [
+	{
+		titulo: "Invasão Silenciosa",
+		subtitulo:
+			"Exploração básica de sistemas de arquivos e listagem de diretórios.",
+		dificuldade: "Easy",
+	},
+	{
+		titulo: "Quebra de Permissões",
+		subtitulo:
+			"Manipulação de privilégios de usuário e modificação de permissões.",
+		dificuldade: "Easy",
+	},
+	{
+		titulo: "Interceptação de Tráfego",
+		subtitulo: "Análise de logs de rede e extração de dados sensíveis.",
+		dificuldade: "Easy",
+	},
+];
+
+const missoes2 = [
+	{
+		titulo: "Varredura Inicial",
+		subtitulo:
+			"Listar arquivos e diretórios usando comandos básicos como ls.",
+		dificuldade: "Easy",
+	},
+	{
+		titulo: "Rastro no Sistema",
+		subtitulo:
+			"Descobrir o diretório atual e navegar entre pastas com pwd e cd.",
+		dificuldade: "Easy",
+	},
+	{
+		titulo: "Arquivo Fantasma",
+		subtitulo:
+			"Criar e visualizar arquivos simples utilizando touch e cat.",
+		dificuldade: "Easy",
+	},
+	{
+		titulo: "Limpeza de Evidências",
+		subtitulo: "Remover arquivos e diretórios com rm e rmdir.",
+		dificuldade: "Easy",
+	},
+	{
+		titulo: "Duplicação de Dados",
+		subtitulo: "Copiar arquivos e pastas usando cp.",
+		dificuldade: "Easy",
+	},
+	{
+		titulo: "Movimentação Tática",
+		subtitulo: "Mover e renomear arquivos com mv.",
+		dificuldade: "Easy",
+	},
+	{
+		titulo: "Leitura de Logs",
+		subtitulo: "Visualizar conteúdos de arquivos grandes com less e head.",
+		dificuldade: "Easy",
+	},
+	{
+		titulo: "Busca na Escuridão",
+		subtitulo: "Encontrar arquivos específicos com find.",
+		dificuldade: "Easy",
+	},
+	{
+		titulo: "Sinal Oculto",
+		subtitulo: "Filtrar informações dentro de arquivos usando grep.",
+		dificuldade: "Easy",
+	},
+	{
+		titulo: "Inventário do Sistema",
+		subtitulo: "Listar processos ativos com ps.",
+		dificuldade: "Easy",
+	},
+	{
+		titulo: "Eco do Terminal",
+		subtitulo: "Exibir mensagens e variáveis com echo.",
+		dificuldade: "Easy",
+	},
+	{
+		titulo: "Arquivo Secreto",
+		subtitulo: "Criar arquivos ocultos e listá-los com ls -a.",
+		dificuldade: "Easy",
+	},
+];
+
+export default missoes2;
